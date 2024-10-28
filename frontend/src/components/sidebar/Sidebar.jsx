77368e368c0afd6db551aa7d6a732e21ed00e745
Pranjal Ghansala <pranjalghansala83@gmail.com>
2024-10-28 17:52:17 +0530

    Updateds it it >> >> >> >> >> >> >> code with latest changes

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
+	const [isOpen, setIsOpen] = useState(true); // State to manage sidebar visibility
+
+	const toggleSidebar = () => {
+		setIsOpen((prev) => !prev);
+	};
+
 	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
+		<div className={`border-r border-slate-500 p-4 flex flex-col transition-all duration-300 ${isOpen ? "w-64" : "w-0 overflow-hidden"}`}>
+			{/* Toggle Button */}
+			<button className="mb-2" onClick={toggleSidebar}>
+				{isOpen ? "Hide" : "Show"} Sidebar
+			</button>
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<Conversations />
@@ -12,21 +23,5 @@ const Sidebar = () => {
 		</div>
 	);
 };
+
 export default Sidebar;
-
-// STARTER CODE FOR THIS FILE
-// import Conversations from "./Conversations";
-// import LogoutButton from "./LogoutButton";
-// import SearchInput from "./SearchInput";
-
-// const Sidebar = () => {
-// 	return (
-// 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-// 			<SearchInput />
-// 			<div className='divider px-3'></div>
-// 			<Conversations />
-// 			<LogoutButton />
-// 		</div>
-// 	);
-// };
-// export default Sidebar;
