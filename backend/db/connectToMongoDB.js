131822b8e22a28a91c8babe426e0390e9a24cc84
Pranjal Ghansala <pranjalghansala83@gmail.com>
2024-10-28 17:02:29 +0530

    Updateds it >> >> >> >> >> code with latest changes

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(process.env.MONGO_DB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			serverSelectionTimeoutMS: 20000, // 20 seconds timeout
+		});
 		console.log("Connected to MongoDB");
 	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
+		console.error("Error connecting to MongoDB:", error.message);
 	}
 };
 
