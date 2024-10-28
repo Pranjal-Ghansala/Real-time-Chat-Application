27de87d4e9749f6b57c99dfe35452aa06a114eae
Pranjal Ghansala <pranjalghansala83@gmail.com>
2024-10-28 22:29:20 +0530

    old

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
-import { Toaster } from "react-hot-toast";
-import { useAuthContext } from "./context/AuthContext";
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
 
-function App() {
-	const { authUser } = useAuthContext();
+const Home = () => {
 	return (
-		<div className='p-4 h-screen flex items-center justify-center'>
-			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
-				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
-			</Routes>
-			<Toaster />
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
 		</div>
 	);
-}
-
-export default App;
+};
+export default Home;
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,25 +1,24 @@
-import Conversation from "./Conversation";
 import useGetConversations from "../../hooks/useGetConversations";
 import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversation";
 
-const Conversations = ({ toggleSidebar }) => {
-  const { loading, conversations } = useGetConversations();
+const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
-  return (
-    <div className='py-2 flex flex-col overflow-auto'>
-      {conversations.map((conversation, idx) => (
-        <div key={conversation._id} onClick={toggleSidebar}>
-          <Conversation
-            conversation={conversation}
-            emoji={getRandomEmoji()}
-            lastIdx={idx === conversations.length - 1}
-          />
-        </div>
-      ))}
-
-      {loading && <span className='loading loading-spinner mx-auto'></span>}
-    </div>
-  );
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
 };
-
 export default Conversations;
+
+ 
